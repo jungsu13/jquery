1ecdb40370a413f802ceb21343dd2f3e9a3b7632
jungsu13 <olympos13@naver.com>
2024-06-13 10:47:29 +0900

    06

---
+++ tutorial/19-ajax/js/common.js
@@ -0,0 +1,25 @@
+$('.header_container').load('../inc/header.html')
+
+// parallax
+$('.banner').parallax({imageSrc: '../../images/slide_01.jpg'});
+
+// gotop
+$('.goTop').on('click', function() {
+    $('html').animate({scrollTop :0}, 'fast')
+})
+
+$(window).on('scroll', function() {
+
+    if($(this).scrollTop() > 200) {
+        $('header').addClass('active')
+    } else {
+        $('header').removeClass('active')
+    }
+
+
+    if($(this).scrollTop() > 500) {
+        $('.goTop').fadeIn()
+    } else {
+        $('.goTop').fadeOut()
+    }
+})
